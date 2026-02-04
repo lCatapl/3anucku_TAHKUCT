0a9468d923539c5de1b278f5361357b9ad0191ff
lCatNapl <nazartrahov1@gmail.com>
2026-02-04 18:10:56 +0300

    Update models.py

--- models.py
+++ models.py
@@ -2,22 +2,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
-import pyotp
 
 db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Integer, unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
-    avatar = db.Column(db.String(200), default='default_tank.png')
-    is_active = db.Column(db.Boolean, default=True)
+    avatar = db.Column(db.String(200), default='t34.png')
+    points = db.Column(db.Integer, default=0)
+    wins = db.Column(db.Integer, default=0)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    
-    # Статистика
-    scores = db.relationship('Score', backref='player', lazy=True)
-    posts = db.relationship('Post', backref='author', lazy=True)
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -28,17 +24,9 @@ class User(UserMixin, db.Model):
 class Tank(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
-    model = db.Column(db.String(50))  # GLTF файл
+    model_file = db.Column(db.String(50))
     hp = db.Column(db.Integer, default=100)
-    speed = db.Column(db.Float, default=1.0)
     damage = db.Column(db.Integer, default=25)
-
-class Score(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    points = db.Column(db.Integer, default=0)
-    wins = db.Column(db.Integer, default=0)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
