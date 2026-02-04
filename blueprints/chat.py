cc5bc388f2e63ebd2d047dda59423ab90ea0f97d
lCatNapl <nazartrahov1@gmail.com>
2026-02-04 19:31:42 +0300

    Update chat.py

--- blueprints/chat.py
+++ blueprints/chat.py
@@ -1,13 +1,7 @@
 from flask import Blueprint, render_template
-from flask_socketio import emit, join_room
 
 chat_bp = Blueprint('chat', __name__)
 
 @chat_bp.route('/chat')
 def chat():
-    return render_template('chat/index.html')
-
-@chat_bp.record_once
-def init_state(state):
-    global socketio
-    socketio = state.app.extensions['socketio']
+    return render_template('chat.html')
