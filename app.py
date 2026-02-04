8c38ff42abe7242a255b82cd3ae464b0c24bf6af
lCatNapl <nazartrahov1@gmail.com>
2026-02-04 19:41:14 +0300

    🔥 Fix Flask-Login user_loader

--- app.py
+++ app.py
@@ -2,14 +2,22 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import DevelopmentConfig
+from models import User
 import os
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
+
+# 🔥 ФИКС: LoginManager + user_loader
+login_manager = LoginManager()
+login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # Blueprints
 from blueprints.auth import auth_bp
@@ -27,6 +35,12 @@ app.register_blueprint(blog_bp)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/init-db')
+def init_db():
+    with app.app_context():
+        db.create_all()
+    return "База данных создана!"
 
 if __name__ == '__main__':
     with app.app_context():
