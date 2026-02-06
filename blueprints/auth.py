d3c73048296cba780729ce8a2305c4493f6cbd63
lCatNapl <nazartrahov1@gmail.com>
2026-02-06 16:47:12 +0300

    Update auth.py

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -1,55 +1,50 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
-from flask_login import login_user, logout_user, login_required, current_user
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from flask import Blueprint, render_template, request, redirect, flash, url_for
+from flask_login import login_user, logout_user, login_required, current_user, login_fresh
+from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, User
 
 auth_bp = Blueprint('auth', __name__)
-
-class LoginForm(FlaskForm):
-    username = StringField('Имя', validators=[DataRequired(), Length(3, 20)])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    submit = SubmitField('Войти')
-
-class RegisterForm(FlaskForm):
-    username = StringField('Имя', validators=[DataRequired(), Length(3, 20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Пароль', validators=[DataRequired(), Length(6)])
-    submit = SubmitField('Зарегистрироваться')
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and user.check_password(form.password.data):
+    
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
             login_user(user)
             return redirect(url_for('index'))
         flash('Неверный логин или пароль')
-    return render_template('auth/login.html', form=form)
+    
+    return render_template('auth/login.html')
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if User.query.filter_by(username=form.username.data).first():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if User.query.filter_by(username=username).first():
             flash('Имя занято')
-        elif User.query.filter_by(email=form.email.data).first():
-            flash('Email занят')
         else:
-            user = User(username=form.username.data, email=form.email.data)
-            user.set_password(form.password.data)
+            user = User(username=username)
+            user.set_password(password)
             db.session.add(user)
             db.session.commit()
             flash('Регистрация успешна!')
             return redirect(url_for('auth.login'))
-    return render_template('auth/register.html', form=form)
-
-@auth_bp.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
+    
+    return '''
+    <!DOCTYPE html>
+    <html><body>
+    <form method="post">
+        Имя: <input name="username"><br>
+        Пароль: <input name="password" type="password"><br>
+        <input type="submit" value="Зарегистрироваться">
+    </form>
+    </body></html>
+    '''
