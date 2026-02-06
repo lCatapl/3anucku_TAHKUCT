088b20531b08b65e52ec02d2142b07c156de9f20
lCatNapl <nazartrahov1@gmail.com>
2026-02-06 20:00:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,8 @@ app.config.from_object(DevelopmentConfig)
 db.init_app(app)
 
 @app.route('/')
-
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')  # Ищет templates/profile.html
+def index():
+    return render_template('index.html')
 
 @app.route('/auth/login')
 def login_page():
@@ -32,7 +30,6 @@ def chat():
     return render_template('chat.html')
 
 @app.route('/profile')
-@login_required
 def profile():
     return render_template('profile.html')
 
@@ -98,6 +95,7 @@ if __name__ == '__main__':
         db.create_all()
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
 
 
 
