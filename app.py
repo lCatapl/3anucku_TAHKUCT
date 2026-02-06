aed17826428bc153bbf7e8b4aa566e352607a979
lCatNapl <nazartrahov1@gmail.com>
2026-02-06 19:28:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,18 +13,14 @@ app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 db.init_app(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'auth.login'
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/auth/login')
+def login_page():
+    return redirect(url_for('auth.login'))
 
 @app.route('/game')
 def game():
@@ -69,3 +65,4 @@ if __name__ == '__main__':
         db.create_all()
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
