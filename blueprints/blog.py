822b740758d2069676cf05b6220eeca37a41e957
lCatNapl <nazartrahov1@gmail.com>
2026-02-08 15:49:39 +0300

    Delete blueprints directory

--- blueprints/blog.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from models import Post
-
-blog_bp = Blueprint('blog', __name__)
-
-@blog_bp.route('/blog')
-def blog():
-    posts = Post.query.all()
-    return render_template('blog/index.html', posts=posts)
