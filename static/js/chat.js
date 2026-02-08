682e0ef656fd4198dfc31ee5a81c82b15a2a820f
lCatNapl <nazartrahov1@gmail.com>
2026-02-08 15:59:57 +0300

    Delete static/js/chat.js

--- static/js/chat.js
+++
@@ -1,6 +0,0 @@
-// Чат функционал
-function sendMessage() {
-    const message = document.getElementById('messageInput').value;
-    socket.emit('chat_message', { message: message });
-    document.getElementById('messageInput').value = '';
-}
