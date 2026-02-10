e464939f7816055919f7791d073c9d79540bd356
lCatNapl <nazartrahov1@gmail.com>
2026-02-10 18:53:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -561,5 +561,5 @@ def not_found(error):
 # ✅ 1.14 ЗАПУСК СЕРВЕРА
 # ========================================
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
